9272feb59eb2a4b402058b1e61c3780737b2e927
salman <slmnkhan2050@gmail.com>
2024-05-08 21:51:42 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,335 @@
+// // Q-2
+
+// let fullName = 'salman'
+
+// console.log(`Hello ${fullName} would you like you lern Python`);
+
+// // Q-3
+
+// let newName = 'salman'
+
+// console.log(newName.toLocaleLowerCase);
+// console.log(newName.toUpperCase);
+// console.log(`${newName.charAt(0).toUpperCase} ${newName.slice(1)}`);
+
+
+// // Q-4
+
+// console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// `);
+
+// // Q-5
+
+// let famousPerson = 'Salman'
+// console.log(`${famousPerson} once said, “A person who never made a mistake never tried anything new.”
+// `);
+
+// // Q-7 & 8
+
+// let num = 20;
+// let num2 = 12
+
+// console.log(num + num2);
+// console.log(num - num2);
+// console.log(num * num2);
+// console.log(num / num2);
+
+
+// // Q-9
+
+// let favNun = 6
+
+// console.log(`My fav num is ${favNun}`);
+
+// // Q-10
+
+// // Fav Num
+
+// let favNun2 = 6
+
+// console.log(`My fav num is ${favNun2}`);
+
+// // Fav Num
+
+// let favNum = 6
+
+// console.log(`My fav num is ${favNum}`);
+
+
+// // Q-11
+
+// let names = ['salman', 'aswad', 'zaid', 'sameer']
+
+// console.log(names);
+
+// // Q-12
+
+// console.log(`My friends name is ${names[0]}`);
+// console.log(`My friends name is ${names[1]}`);
+// console.log(`My friends name is ${names[2]}`);
+// console.log(`My friends name is ${names[3]}`);
+
+// // Q-13
+
+// let cars = ['Corrolla', 'Civic', 'Suzuki', 'Honda']
+
+// console.log(`I would like to own a ${cars[0]}`);
+// console.log(`I would like to own a ${cars[1]}`);
+// console.log(`I would like to own a ${cars[2]}`);
+// console.log(`I would like to own a ${cars[3]}`);
+
+// // Q-14
+
+// let invite = ['salman', 'zaid', 'sameer']
+
+// console.log(`Hello Mr.${invite[0]} you are invited to my birthday party at beach club hotel`); // can't come
+// console.log(`Hello Mr.${invite[1]} you are invited to my birthday party at beach club hotel`);
+// console.log(`Hello Mr.${invite[1]} you are invited to my birthday party at beach club hotel`);
+
+
+
+
+// // Q-15
+
+// invite[0] = 'shariq'
+
+// console.log(`Hello Mr.${invite[0]} you are invited to my birthday party at beach club hotel`);
+// console.log(`Hello Mr.${invite[1]} you are invited to my birthday party at beach club hotel`);
+// console.log(`Hello Mr.${invite[2]} you are invited to my birthday party at beach club hotel`);
+
+// // i am found that the table is bigger than i observed so i need to add some more people
+
+
+// // Q-16
+
+// invite.push('burhan', 'hammad')
+
+// console.log(invite);
+
+// invite.push('Noman')
+// console.log(invite);
+
+// // as per table spces i m only invite 2 persons
+
+// // Q-17
+
+// invite.pop()
+// console.log(invite);
+
+// invite.pop()
+// console.log(invite);
+
+// invite.pop()
+// console.log(invite);
+
+// invite.pop()
+// console.log(invite);
+// // You are still invited
+
+// invite.pop()
+// invite.pop()
+// console.log(invite);
+
+// // Q-18
+
+// let visitPlace = ['Zurich', 'Germany', 'New York', 'Kashmir']
+// console.log(visitPlace);
+// console.log(visitPlace.sort());
+// console.log(visitPlace);
+// console.log(visitPlace.reverse());
+// console.log(visitPlace.reverse());
+// console.log(visitPlace.sort());
+// console.log(visitPlace.reverse());
+
+
+// // Q-19
+
+// console.log(visitPlace.length);
+
+// Q-20
+
+// let counties = ['pakistan', 'india', 'USA']
+// console.log(`${counties[0]} /n ${counties[1]}`);
+
+
+// Q-21
+
+// let myDetails = {
+//     name: 'salman',
+//     age: 30,
+//     married: true,
+//     education: 'MBA'
+// }
+
+// // Q-23
+
+// let car = 'suzuki'
+
+// if(car === 'mehran') {
+//     console.log(`I Predict true`);
+// }
+
+// if(car === 'toyota') {
+//     console.log(`I Predict true`);
+// }
+
+// if(car === 'corolla') {
+//     console.log(`I Predict true`);
+// }
+
+// if(car === 'suzuki') {
+//     console.log(`I Predict true`);
+// }
+
+//  Q-24
+
+// let model = 'suzuki';
+
+// if(model === 'suzuki') {
+//     console.log(`i have car`);
+    
+// } else {
+//     console.log(`dont have car`);
+    
+// }
+
+// // 2nd condition
+// let fruit = 'apple';
+
+// if (fruit === 'Apple') {
+//     console.log(`i have fruit`);
+    
+// } else{
+//     console.log(`dont have fruit`);
+
+// }
+
+
+// 3rd Condition
+// let number = 5;
+
+// if(number === 8) {
+//     console.log(`1st condition is true`);
+// } else if(number > 6) {
+//     console.log('2nd condition is true');
+// } else if (number > 6 ) {
+//     console.log('3rd is true');
+// } else if(number <=4) {
+//     console.log('4th is true');
+// } else if(number >= 5) {
+//     console.log('5th is true');
+// }
+
+
+// Q-25
+
+// let alienColor = 'green'
+
+// if (alienColor == 'green') {
+//     console.log('You have earned 5 points');
+    
+// } else {
+//     console.log();
+    
+// }
+
+
+// Q-26
+
+// let alienColor = 'green'
+
+// if (alienColor == 'green') {
+//     console.log('You have earned 5 points');
+    
+// } else if (alienColor !== 'green') {
+//     console.log('you have earned 10 points');
+// } else {
+//     console.log('no earn points');
+    
+// }
+
+
+// Q-27
+
+// let alienColor = 'green'
+
+// if (alienColor == 'green') {
+//     console.log('You have earned 5 points');
+    
+// } else if (alienColor === 'yellow') {
+//     console.log('you have earned 10 points');
+// } else if (alienColor === 'red') {
+//     console.log('you have earned 15 points');
+// }else {
+//     console.log('no earn points');
+    
+// }
+
+
+
+// Q-28
+
+// let age = 65;
+
+// if(age < 2) {
+//     console.log('person is baby');
+    
+// } else if (age == 2 && age < 4) {
+//     console.log('person is toddler');
+    
+// } else if (age === 4 && age < 13) {
+//     console.log('person is kid');
+    
+// }else if (age === 13 && age < 20) {
+//     console.log('person is teenager');
+    
+// } else if (age === 20 && age < 65) {
+//     console.log('person is teenager');
+
+// } else if (age >= 65) {
+//     console.log('person is elder');
+// }
+
+
+
+// Q-29
+// let favFruit = ['mango', 'graps', 'apple']
+
+// if (favFruit[0] === 'mango') {
+//    console.log(`you really like ${favFruit[0]}`);
+   
+// } else if (favFruit[0] === 'mango') {
+//     console.log(`you really like ${favFruit[0]}`);
+    
+// }else if (favFruit[2] === 'graps') {
+//     console.log(`you really like ${favFruit[2]}`);
+    
+// }else if (favFruit[1] === 'mango') {
+//     console.log(`you really like ${favFruit[1]}`);
+    
+// }
+
+
+
+// Q-30
+
+// let newName = 'admin'
+
+// let userName = ['salman', 'sameer', 'aswad', 'admin']
+
+// for (let i = 0; i < userName.length ; i++) {
+//     if (newName === userName[i]) {
+//         console.log(`Hello admin, would you like to see a status report?`);
+//         break
+        
+//     } else {
+//         console.log(`Hello ${userName[i]} thank you for login`);
+        
+//     }
+    
+// }
+
+
+
+
